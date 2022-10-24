3b8d21e62ffc8065afa9f9c81834c8c9ffe97dbf
mehedihasan2004 <thisismehedihasan0.1@gmail.com>
2022-10-25 00:18:55 +0600

    Project Setup

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+
+const port = process.env.PORT || 5000;
+const datas = require("./data/data.json");
+
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.send("Ice can be fireeeeeeeeeeeeeeeeee");
+});
+
+app.get("/data", (req, res) => {
+  res.send(datas);
+});
+
+app.get("/data/:id", (req, res) => {
+  const id = req.params.id;
+  const data = datas.find((dt) => dt.id == id);
+  res.send(data);
+});
+
+app.listen(port, () => {
+  console.log("Ice fire is running ", port);
+});
