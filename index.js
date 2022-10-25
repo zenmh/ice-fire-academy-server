1ed6bc9061a09a522dcc301e2276ead5415bf2c6
mehedihasan2004 <thisismehedihasan0.1@gmail.com>
2022-10-26 03:11:38 +0600

    Data maked

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ const cors = require("cors");
 const app = express();
 
 const port = process.env.PORT || 5000;
-const datas = require("./data/data.json");
+// const datas = require("./data/data.json");
+const datas = require("./data/courses.json");
 
 app.use(cors());
 
@@ -17,9 +18,15 @@ app.get("/data", (req, res) => {
 
 app.get("/data/:id", (req, res) => {
   const id = req.params.id;
-  const data = datas.find((dt) => dt.id == id);
+  const data = datas.find((dt) => dt._id == id);
   res.send(data);
 });
+
+// app.get("/data/:id", (req, res) => {
+//   const id = req.params.id;
+//   const data = datas.find((dt) => dt.id == id);
+//   res.send(data);
+// });
 
 app.listen(port, () => {
   console.log("Ice fire is running ", port);
